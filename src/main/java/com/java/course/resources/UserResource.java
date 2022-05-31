f592c57d7a647f8a7d7873abdf4e7cbf0c3a11c8
unknown <brunobahr@hotmail.com>
2022-05-30 22:28:36 -0300

    H2 database, test porfile, JPA

--- src/main/java/com/java/course/resources/UserResource.java
+++ src/main/java/com/java/course/resources/UserResource.java
@@ -5,15 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.java.course.entities.User;
+import com.java.course.entities.Users;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User (1L, "Mary", "mary@gmail.com", "999885544", "123456");
+	public ResponseEntity<Users> findAll(){
+		Users u = new Users (1L, "Mary", "mary@gmail.com", "999885544", "123456");
 		return ResponseEntity.ok().body(u);
 	}
 }
---
+++ src/test/java/com/java/course/SpringBootTest.java
@@ -0,0 +1,5 @@
+package com.java.course;
+
+public @interface SpringBootTest {
+
+}
